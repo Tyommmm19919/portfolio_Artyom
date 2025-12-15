774ec8df6cb12192c78be3570f08bb8466d1c7de
Tyommmm19919 <artyom19032008@gmail.com>
2025-12-15 15:02:49 +0400

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_Artyom/', // 👈 имя репозитория
 })
