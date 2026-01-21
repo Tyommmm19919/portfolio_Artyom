216381bc7c5d8cca99d5bd0e08186715aa4ca29a
Tyommmm19919 <artyom19032008@gmail.com>
2026-01-22 03:14:42 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio_Artyom/', // 👈 имя репозитория
-})
+  base: "/portfolio_Artyom/",
+  build: {
+    outDir: "docs"
+  }
+});
